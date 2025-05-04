baf2846b79011e58b55486b321425d7acca1ffb1
Mohamed Ewees <engmohamedewees@gmail.com>
2025-05-04 14:16:56 +0300

    Gold prices 04/05/2025

--- Web_Scraping/BTC_Ingots_price.py
+++ Web_Scraping/BTC_Ingots_price.py
@@ -1,9 +1,8 @@
-# <span class="oe_price" style="white-space: nowrap;" data-oe-type="monetary" data-oe-expression="combination_info['price']"><span class="oe_currency_value">53,936.67</span>&nbsp;LE</span>
-# <span class="oe_currency_value">26,973.34</span>
-# <span class="oe_currency_value">9,456.10</span>
-
 import datetime
 from urllib.request import urlopen
+import requests
+
+
 
 url_10gm = 'https://shop.btcegyptgold.com/shop/10gm-307?category=90#attr=1523,1529,1801'
 url_5gm = 'https://shop.btcegyptgold.com/shop/5gm-335?category=90#attr=1681,1687,1840'
@@ -13,7 +12,7 @@ page_10gm = urlopen(url_10gm)
 page_5gm = urlopen(url_5gm)
 page_coins_quarter = urlopen(url_coin_quarter)
 
-# print(page)
+
 html_bytes_10gm = page_10gm.read()
 html_bytes_5gm = page_5gm.read()
 html_bytes_quarter = page_coins_quarter.read()
@@ -35,8 +34,19 @@ cost_5gm = html_5gm[cost_index_start_5gm:(cost_index_start_5gm+6)]
 cost_index_start_quarter = cost_index_quarter + len('<span class="oe_currency_value">')
 cost_quarter = html_quarter[cost_index_start_quarter:(cost_index_start_quarter + 6)]
 
-print("Current date / time: " + datetime.datetime.now().strftime("%d/%m/%Y , %I:%M:%S %p"))
-print("BTC Ingot(10gm) price: " +cost_10gm+ " LE")
-print("BTC Ingot(5gm) price: " +cost_5gm+ " LE")
-print("BTC Coins(2gm) price: " +cost_quarter+ " LE")
 
+
+message = (("Date/Time: " + datetime.datetime.now().strftime("%d/%m/%Y, %I:%M:%S %p") +
+           "\r\n" + "\u00bb " + "BTC Ingot(10gm) price: " + cost_10gm+ " LE") +
+           "\r\n" + "\u00bb " + "BTC Ingot(5gm) price: " + cost_5gm+ " LE" +
+           "\r\n" + "\u00bb " + "BTC Coins(2gm) price: " + cost_quarter+ " LE")
+
+print("Current date / time: " + datetime.datetime.now().strftime("%d/%m/%Y , %I:%M:%S %p"))
+print("\u00bb " + "BTC Ingot(10gm) price: " +cost_10gm+ " LE")
+print("\u00bb " + "BTC Ingot(5gm) price: " +cost_5gm+ " LE")
+print("\u00bb " + "BTC Coins(2gm) price: " +cost_quarter+ " LE")
+
+token = '7476528415:AAE9Nz8-ZVv78-CwHmeBzHLsFe6BlT3mdYE'
+chat_id = "-4749799913"
+url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+chats = requests.get(url)
--- Web_Scraping/webScrap1.py
+++ Web_Scraping/webScrap1.py
@@ -10,7 +10,12 @@ title_index = html.find("<title>")
 start_index = title_index + len("<title>")
 end_index = html.find("</title>")
 title = html[start_index:end_index]
-print(html)
-print(title_index)
-print(start_index)
-print(title)
+# print(html)
+# print(title_index)
+# print(start_index)
+# print(title)
+
+bold_start = '\033[1m'
+bold_end   = '\033[0m'
+
+print(bold_start, "Hello" , bold_end + "\r\n" +f". Mohamed")
